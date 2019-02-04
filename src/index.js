5625659f78a1b81f9f368029db8e22e5fbf06e88
Joshua Alphonse <joshalphonse@gmail.com>
2019-02-04 14:22:04 -0500

    WISCANSIN OHH LAAAWWD

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const dogUrl = 'http://localhost:3000/dogs'
+    const dogTable = document.querySelector('#table-body')
+    const dogEditForm = document.getElementById('dog-form')
+    console.log(dogTable)
+ 
+    
+    dogTable.addEventListener("click",event => populateEditForm(event,dogEditForm))
+    
+    
+    dogEditForm.addEventListener()
+    
+          
+          
+          
+     fetch(dogUrl)
+    .then(res => res.json())
+    .then(response => displayDogs(response,dogTable))
 
 })
+
+
+function displayDogs(response,table){
+response.forEach(dog =>  
+table.innerHTML += `<tr><td>${dog.name}</td> 
+<td>${dog.breed}</td> 
+<td>${dog.sex}</td> 
+<td><button>Edit</button>
+</td></tr>`    
+)}
+
+
+function populateEditForm(event,form){
+    
+    
+    let tr = event.target.parentNode.parentNode
+    form.name.value = tr.children[0].innerText
+    form.breed.value = tr.children[1].innerText
+    form.sex.value = tr.children[2].innerText
+    
+   
+    
+    
+}
+          
+                 
+                 
